adfccf7058c0b1de47f9169162dd249cb6571803
Max Rataj <maxmilianrataj@gmail.com>
2024-11-16 21:51:07 +0100

    Create SQLiteDbContext

---
+++ Data/SQLiteDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace csharp_EZReserve.Data
+{
+    public class SQLiteDbContext : DbContext
+    {
+        // Retrieve connection string from appSettings.json
+        private readonly string _connectionString;
+
+        public SQLiteDbContext()
+        {
+            var config = AppConfiguration.GetConfiguration();
+            _connectionString = config.GetConnectionString("SqliteConnection")!;
+        }
+
+        // Initiale DbSets for models and set data source
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite("Data Source=CustomerEvidence.db");
+        }
+    }
+}
