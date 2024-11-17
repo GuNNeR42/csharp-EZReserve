0939c6661a10a516a94a3acb376a47399b2e7a46
Max Rataj <maxmilianrataj@gmail.com>
2024-11-17 16:42:31 +0100

    Add Customer factories and seeders

---
+++ Models/Seeders/CustomerSeeder.cs
@@ -0,0 +1,22 @@
+﻿using csharp_EZReserve.Data;
+using csharp_EZReserve.Models.Entities;
+using csharp_EZReserve.Models.Factories;
+
+namespace csharp_EZReserve.Models.Seeders
+{
+    public class CustomerSeeder : BaseSeeder<Customer>
+    {
+        private readonly SQLiteDbContext _context;
+
+        public CustomerSeeder(SQLiteDbContext context) : base(context, new CustomerFactory())
+        {
+            _context = context;
+        }
+
+        public override async Task SeedAsync(int? count = null)
+        {
+            var customers = Factory.Generate(count ?? DefaultCount);
+            await SaveChangesWithIdentityInsertAsync(_context.Customers, customers);
+        }
+    }
+}
