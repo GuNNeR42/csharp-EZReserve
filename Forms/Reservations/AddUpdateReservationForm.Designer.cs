89716e88e9f9ac555abd18cdba7346a1a8a2208d
Max Rataj <maxmilianrataj@gmail.com>
2024-11-17 15:59:01 +0100

    Add forms for reservations CRUD operations

---
+++ Forms/Reservations/AddUpdateReservationForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace csharp_EZReserve.Forms.Reservations
+{
+    partial class AddUpdateReservationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            comboBox_customers = new ComboBox();
+            comboBox_rooms = new ComboBox();
+            dateTimePicker_fromDate = new DateTimePicker();
+            dateTimePicker_toDate = new DateTimePicker();
+            label_customer = new Label();
+            label_room = new Label();
+            label_from = new Label();
+            label_to = new Label();
+            button_save = new Button();
+            errorProvider_validate = new ErrorProvider(components);
+            dateTimePicker_fromTime = new DateTimePicker();
+            dateTimePicker_toTime = new DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)errorProvider_validate).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox_customers
+            // 
+            comboBox_customers.FormattingEnabled = true;
+            comboBox_customers.Location = new Point(90, 27);
+            comboBox_customers.Name = "comboBox_customers";
+            comboBox_customers.Size = new Size(329, 27);
+            comboBox_customers.TabIndex = 0;
+            comboBox_customers.Validating += comboBox_customers_Validating;
+            // 
+            // comboBox_rooms
+            // 
+            comboBox_rooms.FormattingEnabled = true;
+            comboBox_rooms.Location = new Point(90, 60);
+            comboBox_rooms.Name = "comboBox_rooms";
+            comboBox_rooms.Size = new Size(329, 27);
+            comboBox_rooms.TabIndex = 1;
+            comboBox_rooms.Validating += comboBox_rooms_Validating;
+            // 
+            // dateTimePicker_fromDate
+            // 
+            dateTimePicker_fromDate.Location = new Point(90, 93);
+            dateTimePicker_fromDate.Name = "dateTimePicker_fromDate";
+            dateTimePicker_fromDate.Size = new Size(225, 26);
+            dateTimePicker_fromDate.TabIndex = 2;
+            // 
+            // dateTimePicker_toDate
+            // 
+            dateTimePicker_toDate.Location = new Point(90, 125);
+            dateTimePicker_toDate.Name = "dateTimePicker_toDate";
+            dateTimePicker_toDate.Size = new Size(225, 26);
+            dateTimePicker_toDate.TabIndex = 4;
+            // 
+            // label_customer
+            // 
+            label_customer.AutoSize = true;
+            label_customer.Location = new Point(12, 30);
+            label_customer.Name = "label_customer";
+            label_customer.Size = new Size(72, 19);
+            label_customer.TabIndex = 3;
+            label_customer.Text = "Customer:";
+            label_customer.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label_room
+            // 
+            label_room.AutoSize = true;
+            label_room.Location = new Point(36, 63);
+            label_room.Name = "label_room";
+            label_room.Size = new Size(48, 19);
+            label_room.TabIndex = 3;
+            label_room.Text = "Room:";
+            label_room.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label_from
+            // 
+            label_from.AutoSize = true;
+            label_from.Location = new Point(40, 96);
+            label_from.Name = "label_from";
+            label_from.Size = new Size(44, 19);
+            label_from.TabIndex = 3;
+            label_from.Text = "From:";
+            label_from.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label_to
+            // 
+            label_to.AutoSize = true;
+            label_to.Location = new Point(58, 131);
+            label_to.Name = "label_to";
+            label_to.Size = new Size(26, 19);
+            label_to.TabIndex = 3;
+            label_to.Text = "To:";
+            label_to.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // button_save
+            // 
+            button_save.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button_save.Location = new Point(12, 187);
+            button_save.Name = "button_save";
+            button_save.Size = new Size(419, 26);
+            button_save.TabIndex = 100;
+            button_save.Text = "Save";
+            button_save.UseVisualStyleBackColor = true;
+            button_save.Click += button_save_Click;
+            // 
+            // errorProvider_validate
+            // 
+            errorProvider_validate.ContainerControl = this;
+            // 
+            // dateTimePicker_fromTime
+            // 
+            dateTimePicker_fromTime.CustomFormat = "HH:mm";
+            dateTimePicker_fromTime.Format = DateTimePickerFormat.Custom;
+            dateTimePicker_fromTime.Location = new Point(321, 93);
+            dateTimePicker_fromTime.Name = "dateTimePicker_fromTime";
+            dateTimePicker_fromTime.ShowUpDown = true;
+            dateTimePicker_fromTime.Size = new Size(98, 26);
+            dateTimePicker_fromTime.TabIndex = 3;
+            dateTimePicker_fromTime.Validating += dateTimePicker_fromTime_Validating;
+            // 
+            // dateTimePicker_toTime
+            // 
+            dateTimePicker_toTime.CustomFormat = "HH:mm";
+            dateTimePicker_toTime.Format = DateTimePickerFormat.Custom;
+            dateTimePicker_toTime.Location = new Point(321, 124);
+            dateTimePicker_toTime.Name = "dateTimePicker_toTime";
+            dateTimePicker_toTime.ShowUpDown = true;
+            dateTimePicker_toTime.Size = new Size(98, 26);
+            dateTimePicker_toTime.TabIndex = 5;
+            dateTimePicker_toTime.Validating += dateTimePicker_toTime_Validating;
+            // 
+            // AddUpdateReservationForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(443, 225);
+            Controls.Add(dateTimePicker_toTime);
+            Controls.Add(dateTimePicker_fromTime);
+            Controls.Add(button_save);
+            Controls.Add(label_to);
+            Controls.Add(label_from);
+            Controls.Add(label_room);
+            Controls.Add(label_customer);
+            Controls.Add(dateTimePicker_toDate);
+            Controls.Add(dateTimePicker_fromDate);
+            Controls.Add(comboBox_rooms);
+            Controls.Add(comboBox_customers);
+            MinimumSize = new Size(373, 269);
+            Name = "AddUpdateReservationForm";
+            Text = "AddUpdateReservationForm";
+            Load += AddUpdateReservationForm_Load;
+            ((System.ComponentModel.ISupportInitialize)errorProvider_validate).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox_customers;
+        private ComboBox comboBox_rooms;
+        private DateTimePicker dateTimePicker_fromDate;
+        private DateTimePicker dateTimePicker_toDate;
+        private Label label_customer;
+        private Label label_room;
+        private Label label_from;
+        private Label label_to;
+        private Button button_save;
+        private ErrorProvider errorProvider_validate;
+        private DateTimePicker dateTimePicker_fromTime;
+        private DateTimePicker dateTimePicker_toTime;
+    }
+}
---
+++ Forms/Reservations/AddUpdateReservationForm.cs
@@ -0,0 +1,158 @@
+﻿using csharp_EZReserve.Data;
+using csharp_EZReserve.Models.Entities;
+
+namespace csharp_EZReserve.Forms.Reservations
+{
+    public partial class AddUpdateReservationForm : Form
+    {
+        private SQLiteDbContext _dbContext;
+        private Reservation _currentReservation;
+        private bool _updating;
+        private List<Customer> _customers;
+        private List<Reservation> _reservations;
+        private List<string> _rooms = new List<string>
+        {
+            "Room L101",
+            "Room L102",
+            "Room L201",
+            "Room L202",
+            "Room L301",
+            "Room L302"
+        };
+
+        // Ensure minute precision for selected DateTime values
+        private DateTime _selectedFromDateTime =>
+            dateTimePicker_fromDate.Value.Date +
+            TimeSpan.FromMinutes((int)dateTimePicker_fromTime.Value.TimeOfDay.TotalMinutes);
+
+        private DateTime _selectedToDateTime =>
+            dateTimePicker_toDate.Value.Date +
+            TimeSpan.FromMinutes((int)dateTimePicker_toTime.Value.TimeOfDay.TotalMinutes);
+
+        // Get the current time truncated to minute precision
+        private DateTime CurrentTimeTruncated =>
+            DateTime.Now.Date + TimeSpan.FromMinutes((int)DateTime.Now.TimeOfDay.TotalMinutes);
+
+
+        public AddUpdateReservationForm(Reservation currentReservation, SQLiteDbContext dbContext, bool updating = false)
+        {
+            _dbContext = dbContext;
+            _currentReservation = currentReservation;
+            _updating = updating;
+            _customers = _dbContext.Customers.ToList();
+            _reservations = _dbContext.Reservations.ToList();
+
+            InitializeComponent();
+        }
+
+        private void AddUpdateReservationForm_Load(object sender, EventArgs e)
+        {
+            if(_updating && _currentReservation?.Customer != null)
+            {
+                // Restrict the customer change by showing only the current customer in the ComboBox
+                comboBox_customers.DataSource = new List<Customer> { _currentReservation.Customer };
+                comboBox_customers.Enabled = false; // Disable to prevent changes
+            }
+            else
+            {
+                comboBox_customers.DataSource = _customers;
+                comboBox_customers.Enabled = true;
+            }
+            comboBox_customers.DisplayMember = nameof(Customer.FullName);
+            comboBox_customers.ValueMember = nameof(Customer.CustomerId);
+
+            comboBox_rooms.DataSource = _rooms;
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren()) return;
+            if (this.OverlapsWithAnotherReservation())
+            {
+                MessageBox.Show(
+                    "Selected dates overlap with existing reservation for this room",
+                    "Overlapping reservations",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning );
+                return;
+            }
+            _currentReservation.CustomerId = (int)comboBox_customers.SelectedValue!;
+            _currentReservation.Room = (string)comboBox_rooms.SelectedValue!;
+            _currentReservation.From = _selectedFromDateTime;
+            _currentReservation.To = _selectedToDateTime;
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void dateTimePicker_fromTime_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DateTimePicker dtp = (DateTimePicker)sender;
+
+            if (_selectedFromDateTime < CurrentTimeTruncated)
+            {
+                errorProvider_validate.SetError(dtp, "Date must not be in past");
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider_validate.SetError(dtp, "");
+            }
+        }
+
+        private void dateTimePicker_toTime_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DateTimePicker dtp = (DateTimePicker)sender;
+
+            if (_selectedToDateTime < CurrentTimeTruncated)
+            {
+                errorProvider_validate.SetError(dtp, "Date must not be in past");
+                e.Cancel = true;
+            }
+            else if (_selectedToDateTime < _selectedFromDateTime)
+            {
+                errorProvider_validate.SetError(dtp, "Ending date must be after starting date");
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider_validate.SetError(dtp, "");
+            }
+        }
+
+        private void comboBox_customers_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (comboBox_customers.SelectedIndex < 0)
+            {
+                errorProvider_validate.SetError(comboBox_customers, "Choose customer");
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider_validate.SetError(comboBox_customers, "");
+            }
+        }
+
+        private void comboBox_rooms_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (comboBox_rooms.SelectedIndex < 0)
+            {
+                errorProvider_validate.SetError(comboBox_rooms, "Choose room");
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider_validate.SetError(comboBox_rooms, "");
+            }
+        }
+
+        private bool OverlapsWithAnotherReservation()
+        {
+            return _reservations.Any(re =>
+                re.Room == (string)comboBox_rooms.SelectedValue! &&
+                re.ReservationId != _currentReservation.ReservationId &&
+                re.From < _selectedToDateTime && re.To > _selectedFromDateTime
+            );
+        }
+    }
+}
